95c9f38878d934ca781e74fea595c80de1bc2043
yahmed02 <yaseen@aucegypt.edu>
2023-07-27 18:27:44 +0300

    updated projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import code from "../assets/code2.png";
+import riscvImage from "../assets/riscv.png";
+import math_img from "../assets/math.png";
+import server from "../assets/server.jpg";
+import tv from "../assets/tv.jpg";
+import portfolio from "../assets/portfolio.jpg";
 
 const Works = () => {
   return (
@@ -21,156 +26,120 @@ const Works = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(${riscvImage})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
-              <span className=" text-lg font-bold text-white tracking-wider">
-                CBT Application
+              <span className="text-lg font-bold text-white tracking-wider">
+                RISC-V Assembler
               </span>
-              <p className="text-center">
-                A CBT web application built with React and Mongodb
-              </p>
+              <p className="text-center">An open-source RISC-V Assembler</p>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://github.com/yaseenahmed02/RISC-V-Assembler"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    View Project
                   </button>
                 </a>
               </div>
             </div>
           </div>
+
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(${math_img})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
-              <span className=" text-lg font-bold text-white tracking-wider">
-                CBT Application
+              <span className="text-lg font-bold text-white tracking-wider">
+                Linear System Solver
               </span>
               <p className="text-center">
-                A CBT web application built with React and Mongodb
+                A linear-system solver using Cramer's Algorithm
               </p>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://github.com/yaseenahmed02/Cramers-Rule"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    View Project
                   </button>
                 </a>
               </div>
             </div>
           </div>
+
           <div
-            style={{ backgroundImage: `url(${code})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            style={{ backgroundImage: `url(${server})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
-              <span className=" text-lg font-bold text-white tracking-wider">
-                CBT Application
+            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col ">
+              <span className="text-lg font-bold text-white tracking-wider">
+                NetClock
               </span>
-              <p className="text-center">
-                A CBT web application built with React and Mongodb
-              </p>
+              <p className="text-center">A distributed clock server</p>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://github.com/yaseenahmed02/NetClock"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    View Project
                   </button>
                 </a>
               </div>
             </div>
           </div>
+
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(${tv})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
-              <span className=" text-lg font-bold text-white tracking-wider">
-                CBT Application
+              <span className="text-lg font-bold text-white tracking-wider">
+                CheapTalk
               </span>
               <p className="text-center">
-                A CBT web application built with React and Mongodb
+                A server for a retro style messaging application
               </p>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://github.com/yaseenahmed02/CheapTalk"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    View Project
                   </button>
                 </a>
               </div>
             </div>
           </div>
+
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(${portfolio})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
-              <span className=" text-lg font-bold text-white tracking-wider">
-                CBT Application
+              <span className="text-lg font-bold text-white tracking-wider">
+                Portfolio
               </span>
               <p className="text-center">
-                A CBT web application built with React and Mongodb
+                A splendidly chique personal portfolio
               </p>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://github.com/yaseenahmed02/Portfolio"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${code})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col">
-              <span className=" text-lg font-bold text-white tracking-wider">
-                CBT Application
-              </span>
-              <p className="text-center">
-                A CBT web application built with React and Mongodb
-              </p>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
+                    View Project
                   </button>
                 </a>
               </div>
